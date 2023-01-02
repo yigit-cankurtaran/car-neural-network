7ffd5cc33d25d0f6de84d5af99799b2c104f4c8d
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2023-01-02 20:31:02 +0300

    Created the road, on to creating the car.

---
+++ main.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("theCanvas");
+canvas.height = window.innerHeight;
+canvas.width = 200;
+
+const ctx = canvas.getContext("2d");
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
+// we made the car first, now we'll add it to the html
