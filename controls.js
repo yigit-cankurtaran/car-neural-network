b151b12509fd1216c9b49001396d9942caf62fbd
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2023-01-02 20:53:40 +0300

    Added controls, created table in the console

---
+++ controls.js
@@ -0,0 +1,53 @@
+class Controls {
+  // initial constructor for the controls. false because no keys pressed by default
+  constructor() {
+    this.forward = false;
+    this.left = false;
+    this.right = false;
+    this.reverse = false;
+
+    this.#addKeyboardListeners();
+  }
+
+  // method to add the listeners
+  // hashtag in front because PRIVATE method.
+  // can NOT be accessed outside the Controls class
+  #addKeyboardListeners() {
+    // the tutorial uses "event" for this, but event is deprecated.
+    document.onkeydown = (Event) => {
+      switch (Event.key) {
+        case "ArrowLeft":
+          this.left = true;
+          break;
+        case "ArrowRight":
+          this.right = true;
+          break;
+        case "ArrowUp":
+          this.forward = true;
+          break;
+        case "ArrowDown":
+          this.reverse = true;
+          break;
+      }
+      console.table(this);
+    };
+
+    document.onkeyup = (Event) => {
+      switch (Event.key) {
+        case "ArrowLeft":
+          this.left = false;
+          break;
+        case "ArrowRight":
+          this.right = false;
+          break;
+        case "ArrowUp":
+          this.forward = false;
+          break;
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+      }
+      console.table(this);
+    };
+  }
+}
