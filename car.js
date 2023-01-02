ee61b1832cac2aecdc5cd75572ac711e541a77d0
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2023-01-02 20:37:19 +0300

    Created the car object.

---
+++ car.js
@@ -0,0 +1,21 @@
+class Car {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.rect(
+      // the car will be a simple rectangle
+      this.x - this.width / 2,
+      // the x is going to be in the center of the car
+      this.y - this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.fill();
+  }
+}
