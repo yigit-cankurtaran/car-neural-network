3e6fecf315dc7eb0adf675deee98851607c2b11a
yigit-cankurtaran <yigitcankurtaranyc@gmail.com>
2023-02-08 14:43:57 +0300

    Implemented controls. The car's old state is still visible. Will fix.

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
 car.draw(ctx);
 // we made the car first, now we'll add it to the html
+
+animate();
+
+function animate() {
+  car.update();
+  car.draw(ctx);
+  requestAnimationFrame(animate);
+}
