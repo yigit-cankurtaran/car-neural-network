1c0e692f7a323e4bafe8fde221b76906c50536a5
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-02-21 17:57:22 +0300

    Modified gitignore, fixing the car multiplication issue

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
 // we made the car first, now we'll add it to the html
 
 animate();
