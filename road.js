0d2e968f37c7cc903491791244e8f7a77f3929b1
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-02-22 19:05:07 +0300

    Created utils, moved lerp function in it

--- road.js
+++ road.js
@@ -33,10 +33,3 @@ class Road {
     }
   }
 }
-
-function lerp(A, B, t) {
-  return A + (B - A) * t;
-  // linear interpolation
-  // zero or one, 0% or 100%
-  // move to utils later
-}
---
+++ utils.js
@@ -0,0 +1,6 @@
+function lerp(A, B, t) {
+  return A + (B - A) * t;
+  // linear interpolation
+  // zero or one, 0% or 100%
+  // move to utils later
+}
