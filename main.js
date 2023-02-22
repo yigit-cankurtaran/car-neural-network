21217907b65a26889550a065a0131d31ce092942
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-02-22 19:00:35 +0300

    Finished drawing the lanes on the sides of the road. Moving in for the actual road.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById("theCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+// we made the width a bit smaller to give the ends a little margin
 const car = new Car(100, 100, 30, 50);
 // car.draw(ctx);
 // commented because we will add it to the function.
@@ -15,6 +17,7 @@ function animate() {
   canvas.height = window.innerHeight;
   // moving this here makes it so that it updates with every drawing
   // also fixes the issue of the car being stretched on the screen
+  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
---
+++ road.js
@@ -0,0 +1,42 @@
+class Road {
+  constructor(x, width, laneCount = 3) {
+    this.x = x;
+    this.width = width;
+    this.laneCount = laneCount;
+
+    this.left = x - width / 2;
+    this.right = x + width / 2;
+
+    const infinity = 200000;
+    //   when drawing things with infinite size, weird things might happen
+    //   might be subject to change
+    //   was subject to change. changed from Infinity to 200000
+
+    this.top = -infinity;
+    this.bottom = infinity;
+  }
+
+  draw(ctx) {
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "white";
+
+    for (let i = 0; i <= this.laneCount; i++) {
+      const x = lerp(this.left, this.right, i / this.laneCount);
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
+      //   drawing lines on the left
+      // i is the lane count the end is gonna be 1, the rest is gonna be percentages
+      // lerp is a function that takes two numbers and returns a number between them
+      const y = lerp(this.top, this.bottom, i / this.laneCount);
+    }
+  }
+}
+
+function lerp(A, B, t) {
+  return A + (B - A) * t;
+  // linear interpolation
+  // zero or one, 0% or 100%
+  // move to utils later
+}
