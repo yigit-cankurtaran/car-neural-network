543b3dd9556254be9740af25bc3f8c75903f4855
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-02-23 11:52:22 +0300

    Created the rays for the sensor. Now I'll move to implementing them.

--- sensor.js
+++ sensor.js
@@ -25,6 +25,17 @@ class Sensor {
         y: this.car.y - Math.cos(rayAngle) * this.rayLength,
       };
       this.rays.push([start, end]);
+      // defining a segment. like how we did with the lanes
+    }
+  }
+  draw(ctx) {
+    for (let i = 0; i < this.rayCount; i++) {
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "yellow";
+      ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
+      ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.stroke();
     }
   }
 }
