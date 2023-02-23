5078860974491dfa234e828e4f2ce6d81429d52d
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-02-23 14:08:33 +0300

    Implemented another car. Now we're controlling the car we just spawned instead of our own car. Will fix.

--- main.js
+++ main.js
@@ -10,9 +10,16 @@ const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 // the function will call itself again and again.
 // we made the car first, now we'll add it to the html
 
+// traffic is going to be an array of cars.
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50)];
+
 animate();
 
 function animate() {
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders);
+  }
+  // above code updates the other chars as well
   car.update(road.borders);
   canvas.height = window.innerHeight;
   // moving this here makes it so that it updates with every drawing
@@ -20,6 +27,10 @@ function animate() {
   ctx.save();
   ctx.translate(0, -car.y + canvas.height * 0.7);
   road.draw(ctx);
+  for(let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(ctx);
+  }
+  // above code will draw each car on the canvas
   car.draw(ctx);
   // centers the camera on the car, puts the car on the lower part of the screen
   ctx.restore();
