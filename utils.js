3ffe803df0246abe772002136f3497d19462ac73
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-02-23 12:26:21 +0300

    Implemented the getIntersection function, still doesn't work properly. Will fix.

--- utils.js
+++ utils.js
@@ -4,3 +4,22 @@ function lerp(A, B, t) {
   // zero or one, 0% or 100%
   // move to utils later
 }
+
+function getIntersection(A, B, C, D) {
+  const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
+  const uTop = (C.y - A.y) * (A.x - C.x) - (C.x - A.x) * (A.y - C.y);
+  const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
+  if (bottom != 0) {
+    const t = tTop / bottom;
+    const u = uTop / bottom;
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
+      return {
+        x: lerp(A.x, B.x, t),
+        y: lerp(A.y, B.y, t),
+        offset: t
+      };
+    }
+  }
+
+  return null;
+}
