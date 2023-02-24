fb738094f0e91b93ed6de7f047249bf20f883b5d
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-02-24 16:36:35 +0300

    Finished the Level class in the network file.

--- network.js
+++ network.js
@@ -25,19 +25,25 @@ class Level {
       }
     }
     for (let i = 0; i < level.biases.length; i++) {
-        level.biases[i] = Math.random() * 2 - 1;
+      level.biases[i] = Math.random() * 2 - 1;
     }
   }
-    
-  static feedForward(givenInputs, level){
+
+  static feedForward(givenInputs, level) {
     for (let i = 0; i < level.inputs.length; i++) {
       level.inputs[i] = givenInputs[i];
     }
     for (let i = 0; i < level.outputs.length; i++) {
       let sum = 0;
       for (let j = 0; j < level.inputs.length; j++) {
-        // sum += level.inputs[j] * level.weights[0)
-  }
-}
+        sum += level.inputs[j] * level.weights[j][i];
+      }
+      if (sum > level.biases[i]) {
+        level.outputs[i] = 1;
+      } else {
+        level.outputs[i] = 0;
+      }
+    }
+    return level.outputs;
   }
 }
