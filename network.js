17a0cc2c537601d9f1ae59b0f7378545f3d53f26
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-02-25 12:37:34 +0300

    Another unexplainable error. Rewrote the original code.

--- network.js
+++ network.js
@@ -1,70 +1,68 @@
-// create a class for a neural network
-//
-
-class NeuralNetwork {
-  constructor(neuronCounts) {
-    this.levels = [];
-    // our neural network will be an array of levels
-    for (let i = 0; i < neuronCounts.length - 1; i++) {
-      this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
+class NeuralNetwork{
+    constructor(neuronCounts){
+        this.levels=[];
+        for(let i=0;i<neuronCounts.length-1;i++){
+            this.levels.push(new Level(
+                neuronCounts[i],neuronCounts[i+1]
+            ));
+        }
     }
-  }
 
-  static feedForward(givenInputs, network) {
-    let outputs = Level.feedForward(givenInputs, network.levels[0]);
-    for (let i = 1; i < network.levels.length; i++) {
-      outputs = Level.feedForward(outputs, network.levels[i]);
+    static feedForward(givenInputs,network){
+        let outputs=Level.feedForward(
+            givenInputs,network.levels[0]);
+        for(let i=1;i<network.levels.length;i++){
+            outputs=Level.feedForward(
+                outputs,network.levels[i]);
+        }
+        return outputs;
     }
-    return outputs;
-  }
 }
 
-class Level {
-  constructor(inputCount, outputCount) {
-    // layer of input neurons, layer of output neurons
-    // we can implement those as arrays
+class Level{
+    constructor(inputCount,outputCount){
+        this.inputs=new Array(inputCount);
+        this.outputs=new Array(outputCount);
+        this.biases=new Array(outputCount);
 
-    this.inputs = new Array(inputCount);
-    this.outputs = new Array(outputCount);
-    // bias = the value above the neuron will fire
-    this.biases = new Array(outputCount);
-    //   we will connect every input neuron to every output neuron
-    //
+        this.weights=[];
+        for(let i=0;i<inputCount;i++){
+            this.weights[i]=new Array(outputCount);
+        }
 
-    this.weights = [];
-    for (let i = 0; i < inputCount; i++) {
-      this.weights[i] = new Array(outputCount);
+        Level.#randomize(this);
     }
-    Level.#randomize(this);
-  }
-  static #randomize(level) {
-    for (let i = 0; i < level.inputs.length; i++) {
-      for (let j = 0; j < level.outputs.length; j++) {
-        level.weights[i][j] = Math.random() * 2 - 1;
-        //   easy way to get a value between -1 and 1
-        // negative weights can send messages to stop, positives can send messages to go
-      }
-    }
-    for (let i = 0; i < level.biases.length; i++) {
-      level.biases[i] = Math.random() * 2 - 1;
-    }
-  }
 
-  static feedForward(givenInputs, level) {
-    for (let i = 0; i < level.inputs.length; i++) {
-      level.inputs[i] = givenInputs[i];
+    static #randomize(level){
+        for(let i=0;i<level.inputs.length;i++){
+            for(let j=0;j<level.outputs.length;j++){
+                level.weights[i][j]=Math.random()*2-1;
+            }
+        }
+
+        for(let i=0;i<level.biases.length;i++){
+            level.biases[i]=Math.random()*2-1;
+        }
     }
-    for (let i = 0; i < level.outputs.length; i++) {
-      let sum = 0;
-      for (let j = 0; j < level.inputs.length; j++) {
-        sum += level.inputs[j] * level.weights[j][i];
-      }
-      if (sum > level.biases[i]) {
-        level.outputs[i] = 1;
-      } else {
-        level.outputs[i] = 0;
-      }
+
+    static feedForward(givenInputs,level){
+        for(let i=0;i<level.inputs.length;i++){
+            level.inputs[i]=givenInputs[i];
+        }
+
+        for(let i=0;i<level.outputs.length;i++){
+            let sum=0
+            for(let j=0;j<level.inputs.length;j++){
+                sum+=level.inputs[j]*level.weights[j][i];
+            }
+
+            if(sum>level.biases[i]){
+                level.outputs[i]=1;
+            }else{
+                level.outputs[i]=0;
+            } 
+        }
+
+        return level.outputs;
     }
-    return level.outputs;
-  }
 }
